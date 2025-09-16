c5255084162fbf3ab1bffd5123ce52bc8bcd09dd
jt-1105 <minataher0511@gmail.com>
2025-09-16 14:42:58 +0200

    list med alle data

--- toejbutik-app/src/DataItem.jsx
+++ toejbutik-app/src/DataItem.jsx
@@ -1,5 +1,5 @@
 export default function DataItem(props) {
     return (
-        <h2>{props.data.brand}</h2>
+            <h2>{props.data.brand}{props.data.model}{props.data.size}{props.data.price}{props.data.color}{props.data.description}</h2>
     )
 }
