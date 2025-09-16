a753a08a702ab2b4644d559230f1919e8f7caca3
jt-1105 <minataher0511@gmail.com>
2025-09-16 14:39:08 +0200

    glemt data

--- toejbutik-app/src/App.js
+++ toejbutik-app/src/App.js
@@ -6,8 +6,8 @@ import ItemList from './ItemList';
 function App() {
   // Vi 'leger' at vi har hentet disse data fra serveren og databasen.
   const items = [
-        new Data(1, 'mærke', 'xs', '150', 'red', 'desc'),
-        new Data(2, 'mærke2', 's', '175', 'pink', 'desc'),
+        new Data(1, 'mærke', 'model', 'xs', '150', 'red', 'desc'),
+        new Data(2, 'mærke2', 'model2', 's', '175', 'pink', 'desc'),
     ]
 
   return (
