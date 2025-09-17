99622c886c4a0d76c0eccdf0aa2fb3066fa9e96f
jt-1105 <minataher0511@gmail.com>
2025-09-17 11:01:37 +0200

    react styling removed

--- toejbutik-app/src/App.js
+++ toejbutik-app/src/App.js
@@ -13,18 +13,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         
         <ItemList dataItems={items}/>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- toejbutik-app/src/DataItem.jsx
+++ toejbutik-app/src/DataItem.jsx
@@ -5,6 +5,5 @@ export default function DataItem(props) {
                 <li>{props.data.brand} - {props.data.model} - {props.data.size} - {props.data.price} DKK - {props.data.color} - {props.data.description}</li>
             </ul>
         </div>
-        // <h2>{props.data.brand} - {props.data.model} - {props.data.size} - {props.data.price} DKK - {props.data.color} - {props.data.description}</h2>
       );
     }
