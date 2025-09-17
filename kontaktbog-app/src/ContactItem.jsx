4e58810f17f2279afba81779367cbe0a659049e1
Mariawashere <cph-mb1046@cphbusiness.dk>
2025-09-17 10:54:25 +0200

    h2 fix

--- kontaktbog-app/src/ContactItem.jsx
+++ kontaktbog-app/src/ContactItem.jsx
@@ -2,6 +2,14 @@ export default function ContactItem(props) {
     console.log(props);
     
     return (
-        <h2>{props.contact.firstname} {props.contact.lastname}</h2>
+        <div>
+            <h2>{props.contact.firstname}</h2>
+            <h2>{props.contact.lastname}</h2>
+            <h2>{props.contact.email}</h2>
+            <h2>{props.contact.phone}</h2>
+            <h2>{props.contact.company}</h2>
+            <h2>{props.contact.position}</h2>
+        </div>
+        
     )
 }
