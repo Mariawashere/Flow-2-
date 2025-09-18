bfef9c458fc554455300dc5bd594667d74fb699f
Mariawashere <cph-mb1046@cphbusiness.dk>
2025-09-18 13:30:52 +0200

    jeg har lavet produkt kort til tøjbutikken + inline style, så I kan sætte billeder i højre side

--- toejbutik-app/src/DataItem.jsx
+++ toejbutik-app/src/DataItem.jsx
@@ -1,4 +1,24 @@
 export default function DataItem(props) {
+// dataItem er en komponent der modtager et produkt (et data-objekt)
+
+     const DataItem = [  // her er vores data som vi 'henter' fra databasen/serveren
+  {
+    brand: "Gentlewoman",
+    model: "Model1",
+    size: "XS",
+    price: 150,
+    color: "Red",
+    description: "Elegant and minimalistic"
+  },
+  {
+    brand: "Gentlewoman",
+    model: "Model2",
+    size: "S",
+    price: 175,
+    color: "Pink",
+    description: "Soft and feminine"
+  }
+];
       return (
         <div>
             <ul>
--- toejbutik-app/src/ItemList.jsx
+++ toejbutik-app/src/ItemList.jsx
@@ -1,7 +1,4 @@
 import DataItem from "./DataItem.jsx";
-
-
-
 export default function ItemList(props) {
     
     return (
@@ -14,5 +11,35 @@ export default function ItemList(props) {
             )
         })}
     </div>
+
   )
+
+  function DataItem({ data}) {  //  her laver vi inline styling ift vores product-cards/ 
+    //denne komponent modtager en liste af data (dataItems) og viser dem ved at loope igennem og gengive DataItem komponenten for hver enkelt data objekt.
+  return ( 
+
+    <div style={{  //layout styling/
+      border: "1px solid #ccc",
+      borderRadius: "8px",
+      padding: "20px",
+      marginBottom: "12px",
+      boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+      width: "250px",
+      backgroundColor: "#fff",
+      display:"flex",
+      flexWrap: "wrap",
+      gap: "20px",
+      justifyContent: "center",
+    }}> 
+    
+      <h2 style={{ marginBottom: "8px" }}>{data.marke}</h2>  
+      <p><strong>Model:</strong> {data.model}</p>
+      <p><strong>Size:</strong> {data.size}</p>
+      <p><strong>Price:</strong> {data.price} DKK</p>
+      <p><strong>Color:</strong> {data.color}</p>
+      <p><strong>Description:</strong> {data.desc}</p>
+    </div>
+  );
+}
+
 }
