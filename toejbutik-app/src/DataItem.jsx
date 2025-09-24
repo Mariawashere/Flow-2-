3d9055b907e40658b39587180bb0247988779ea6
jt-1105 <minataher0511@gmail.com>
2025-09-24 10:22:21 +0200

    cards

--- toejbutik-app/src/DataItem.jsx
+++ toejbutik-app/src/DataItem.jsx
@@ -1,22 +1,41 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 function DataItem({ item, addToCart }) {
         return (
-          <div className="card h-100">
-            <div className="card-body">
-              <h5 className="card-title">{item.brand} - {item.model}</h5>
-              <p className="card-text">Size: {item.size}</p>
-              <p className="card-text">Color: {item.color}</p>
-              <p className="card-text">{item.description}</p>
-              <h6 className="card-subtitle mb-2 text-muted">{item.price} kr.</h6>
-              <button
-                className="btn btn-primary"
-                onClick={() => addToCart(item.id)}>
-
-                Add to cart
-
-              </button>
+            <div>
+                <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>{item.brand} - {item.model}</Card.Title>
+                        <Card.Text>
+                            <p className="card-text">Size: {item.size}</p>
+                            <p className="card-text">Color: {item.color}</p>
+                            <p className="card-text">{item.description}</p>
+                            <h6 className="card-subtitle mb-2 text-muted">{item.price} kr.</h6>
+                        </Card.Text>
+                        <Button variant="primary" onClick={() => addToCart(item.id)}>Add to cart</Button>
+                    </Card.Body>
+                </Card>
             </div>
-          </div>
+            
+        //   <div className="card h-100">
+        //     <div className="card-body">
+        //       <h5 className="card-title">{item.brand} - {item.model}</h5>
+        //       <p className="card-text">Size: {item.size}</p>
+        //       <p className="card-text">Color: {item.color}</p>
+        //       <p className="card-text">{item.description}</p>
+        //       <h6 className="card-subtitle mb-2 text-muted">{item.price} kr.</h6>
+        //       <button
+        //         className="btn btn-primary"
+        //         onClick={() => addToCart(item.id)}>
+
+        //         Add to cart
+
+        //       </button>
+        //     </div>
+        //   </div>
         );
       }
       
-      export default DataItem;
+export default DataItem;
