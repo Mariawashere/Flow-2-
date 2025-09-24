48d48902e6a09bfe781c7b4e77d5d51a4a9bae32
jt-1105 <minataher0511@gmail.com>
2025-09-24 10:40:25 +0200

    card fix

--- toejbutik-app/src/DataItem.jsx
+++ toejbutik-app/src/DataItem.jsx
@@ -40,23 +40,7 @@ function DataItem({ item, addToCart }) {
 
               </button>
             </div>
-            
-        //   <div className="card h-100">
-        //     <div className="card-body">
-        //       <h5 className="card-title">{item.brand} - {item.model}</h5>
-        //       <p className="card-text">Size: {item.size}</p>
-        //       <p className="card-text">Color: {item.color}</p>
-        //       <p className="card-text">{item.description}</p>
-        //       <h6 className="card-subtitle mb-2 text-muted">{item.price} kr.</h6>
-        //       <button
-        //         className="btn btn-primary"
-        //         onClick={() => addToCart(item.id)}>
-
-        //         Add to cart
-
-        //       </button>
-        //     </div>
-        //   </div>
+        </div>
         );
       }
       
